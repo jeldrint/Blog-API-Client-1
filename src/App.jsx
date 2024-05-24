cbdd2dc4544428294770aa99414fe1bd1c469a14
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-05-24 19:19:24 +0800

    minor changes for client

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/* function App() {
   const [data, setData] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
         const data = await fetch('/techy-blog/api');
         const response = await data.json();
-        setData(response.message)
+        setData(response.user)
     }
     fetchData();
   },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p>{!data ? "Loading..." : data}</p>
+      <p>{!data ? "Loading..." : data.first_name}</p>
     </>
   )
-}
+} */
+
+const App = () => {
+
+    return(
+      <div className="h-screen w-screen flex flex-col items-center">
+        <h1>Hello!</h1>
+      </div>
+  )
+  }
 
 export default App
