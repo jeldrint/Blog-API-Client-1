b8a28869fb4926c1e64e81d817ed25528ea8ab36
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-06-07 21:29:34 +0800

    admin interface is done (with write, delete, and update post buttons)

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,35 +1,69 @@
 
-
-
-
 const Blog = ({mainData}) => {
-    //console.log(mainData.posts)
-
-    return(
-        <div className="h-full w-full p-4 gap-y-2">
-            {mainData.posts.map(post => {
-                const timestamp = new Date(post.timestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
-                const updatedTimestamp = new Date(post.updatedTimestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
-                return(
-                        <div key={post._id} className="mx-2 mb-10 p-6 rounded-md shadow-lg border-2 font-myFont flex-col">
-                            <div>
-                                <div className="font-bold tracking-wide text-xl md:text-2xl lg:text-3xl xl:text-4xl ">{post.title}</div>
-                                <div className="text-xs lg:text-sm flex-col mt-[5px]">
-                                    <div className='flex gap-x-[50px]'><span>BY: <strong><i>{post.userId.first_name} {post.userId.family_name}</i></strong></span> <span>Created: {timestamp}</span></div>
-                                    { post.isUpdated && <div className='flex gap-x-4'><span>MODIFIED BY: <strong><i>{post.userIdUpdated.first_name} {post.userIdUpdated.family_name}</i></strong></span> <span>Created: {updatedTimestamp}</span></div> }
+    //console.log(mainData.user.isAdmin)
+    //{mainData.user.isAdmin ? <div>WRITE A POST</div> : <></>}
+    
+    if(mainData.user){
+        return(
+            <div className="h-full w-full p-4 gap-y-2">
+                {mainData.user.isAdmin && <button className="w-full mb-2 text-[15px] rounded-md p-[6px] bg-sky-500 transition duration-200 hover:bg-sky-600 text-neutral-50">WRITE A POST</button>}
+                {mainData.posts.map(post => {
+                    const timestamp = new Date(post.timestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
+                    const updatedTimestamp = new Date(post.updatedTimestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
+                    return(
+                            <div key={post._id} className="mx-2 mb-10 p-6 rounded-md shadow-lg border-2 font-myFont flex-col">
+                                <div>
+                                    <div className="grid grid-cols-4">
+                                        <div className="col-span-3 font-bold tracking-wide text-xl md:text-2xl lg:text-3xl xl:text-4xl ">{post.title}</div>
+                                        <div className="flex flex-col md:flex-row items-center justify-center gap-x-2 gap-y-2 text-sm lg:text-base">
+                                            <button className="rounded py-1 px-3 bg-slate-500 transition duration-200 hover:bg-sky-600 text-neutral-50">UPDATE</button>
+                                            <button className="rounded py-1 px-3 bg-slate-500 transition duration-200 hover:bg-sky-600 text-neutral-50">DELETE</button>
+                                        </div>
+                                    </div>
+                                    <div className="text-xs lg:text-sm flex-col mt-[5px]">
+                                        <div className='flex gap-x-[50px]'><span>BY: <strong><i>{post.userId.first_name} {post.userId.family_name}</i></strong></span> <span>Created: {timestamp}</span></div>
+                                        { post.isUpdated && <div className='flex gap-x-4'><span>MODIFIED BY: <strong><i>{post.userIdUpdated.first_name} {post.userIdUpdated.family_name}</i></strong></span> <span>Modified: {updatedTimestamp}</span></div> }
+                                    </div>
+                                </div>
+                                <br />
+                                <p className="whitespace-pre-wrap text-sm md:text-[15px] lg:text-base">{post.body}</p>
+                                <br />
+                                <div>
+                                    <button className="text-sm rounded-md p-[6px] bg-sky-500 transition duration-200 hover:bg-sky-600 text-neutral-50">ADD A COMMENT</button>
                                 </div>
                             </div>
-                            <br />
-                            <p className="whitespace-pre-wrap text-sm md:text-[15px] lg:text-base">{post.body}</p>
-                            <br />
-                            <div>
-                                <button className="text-sm rounded-md p-[6px] bg-sky-500 transition duration-200 hover:bg-sky-600 text-neutral-50">ADD A COMMENT</button>
+                    )
+                })}
+            </div>
+        )
+    }else{
+        return(
+            <div className="h-full w-full p-4 gap-y-2">
+                {mainData.posts.map(post => {
+                    const timestamp = new Date(post.timestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
+                    const updatedTimestamp = new Date(post.updatedTimestamp).toLocaleDateString('en-US',{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'})
+                    return(
+                            <div key={post._id} className="mx-2 mb-10 p-6 rounded-md shadow-lg border-2 font-myFont flex-col">
+                                <div>
+                                    <div className="font-bold tracking-wide text-xl md:text-2xl lg:text-3xl xl:text-4xl ">{post.title}</div>
+                                    <div className="text-xs lg:text-sm flex-col mt-[5px]">
+                                        <div className='flex gap-x-[50px]'><span>BY: <strong><i>{post.userId.first_name} {post.userId.family_name}</i></strong></span> <span>Created: {timestamp}</span></div>
+                                        { post.isUpdated && <div className='flex gap-x-4'><span>MODIFIED BY: <strong><i>{post.userIdUpdated.first_name} {post.userIdUpdated.family_name}</i></strong></span> <span>Created: {updatedTimestamp}</span></div> }
+                                    </div>
+                                </div>
+                                <br />
+                                <p className="whitespace-pre-wrap text-sm md:text-[15px] lg:text-base">{post.body}</p>
+                                <br />
+                                <div>
+                                    <button className="text-sm rounded-md p-[6px] bg-sky-500 transition duration-200 hover:bg-sky-600 text-neutral-50">ADD A COMMENT</button>
+                                </div>
                             </div>
-                        </div>
-                )
-            })}
-        </div>
-    )
+                    )
+                })}
+            </div>
+        )
+    }
+
 }
     
 export default Blog;
